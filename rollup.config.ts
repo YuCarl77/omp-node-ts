0db0dcbb3dbeb759547fb448fb355aeb167d6175
yucarl77 <yucarl77@foxmail.com>
2022-07-25 12:35:56 +0800

    chore: rollup auto externals

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,16 +1,18 @@
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
+import externals from "rollup-plugin-node-externals";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
 const isDev = process.env.NODE_ENV === "dev";
 
 const plugins = [
+  externals(),
+  nodeResolve(),
   typescript({ tsconfig: "./tsconfig.json" }),
   json(),
   commonjs(),
-  nodeResolve(),
 ];
 
 if (!isDev) plugins.push(terser());
@@ -23,5 +25,4 @@ export default {
     sourcemap: isDev,
   },
   plugins,
-  external: ["samp-node-lib"],
 };
