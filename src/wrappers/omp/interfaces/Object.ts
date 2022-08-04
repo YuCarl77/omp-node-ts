81580bd56d778a122849712ce23335b23e6caa24
yucarl77 <yucarl77@foxmail.com>
2022-08-04 10:33:59 +0800

    feat: omp object global wrapper

--- src/wrappers/omp/interfaces/Object.ts
+++ src/wrappers/omp/interfaces/Object.ts
@@ -1,15 +1,44 @@
-export interface AttachedObjectImpl {
-  modelid: number;
-  bone: number;
+export interface ObjectPosImpl {
   fX: number;
   fY: number;
   fZ: number;
+}
+
+export interface ObjectRotPosImpl extends ObjectPosImpl {
   fRotX: number;
   fRotY: number;
   fRotZ: number;
+}
+export interface AttachedObjectImpl extends ObjectRotPosImpl {
+  modelid: number;
+  bone: number;
   fScaleX: number;
   fScaleY: number;
   fScaleZ: number;
   materialcolor1: number;
   materialcolor2: number;
+}
+
+export interface MaterialImpl {
+  modelid: number;
+  txdname: string;
+  texturename: string;
+  materialcolor: string;
+}
+
+export interface MaterialTextImpl {
+  text: string;
+  materialsize: number;
+  fontface: string;
+  fontsize: number;
+  bold: number;
+  fontcolor: string;
+  backcolor: string;
+  textalignment: number;
+}
+
+export interface AttachedDataImpl {
+  attached_vehicleid: number;
+  attached_objectid: number;
+  attached_playerid: number;
 }
--- src/wrappers/omp/object/global.ts
+++ src/wrappers/omp/object/global.ts
@@ -1,1 +1,149 @@
-export {};
+import {
+  AttachedDataImpl,
+  MaterialImpl,
+  MaterialTextImpl,
+  ObjectPosImpl,
+  ObjectRotPosImpl,
+} from "../interfaces/Object";
+
+export const GetObjectDrawDistance = (objectid: number): number => {
+  return samp.callNativeFloat("GetObjectDrawDistance", "i", objectid);
+};
+
+export const GetObjectMoveSpeed = (objectid: number): number => {
+  return samp.callNativeFloat("GetObjectMoveSpeed", "i", objectid);
+};
+
+export const GetObjectTarget = (objectid: number): ObjectPosImpl => {
+  const [fX = 0.0, fY = 0.0, fZ = 0.0]: number[] = samp.callNative(
+    "GetObjectTarget",
+    "i",
+    objectid
+  );
+  return { fX, fY, fZ };
+};
+
+export const GetObjectMovingTargetPos = (objectid: number): ObjectPosImpl => {
+  const [fX = 0.0, fY = 0.0, fZ = 0.0]: number[] = samp.callNative(
+    "GetObjectMovingTargetPos",
+    "i",
+    objectid
+  );
+  return { fX, fY, fZ };
+};
+
+export const GetObjectMovingTargetRot = (objectid: number): ObjectPosImpl => {
+  const [fX = 0.0, fY = 0.0, fZ = 0.0]: number[] = samp.callNative(
+    "GetObjectMovingTargetRot",
+    "i",
+    objectid
+  );
+  return { fX, fY, fZ };
+};
+
+export const GetObjectAttachedData = (objectid: number): AttachedDataImpl => {
+  const [
+    attached_vehicleid = 0,
+    attached_objectid = 0,
+    attached_playerid = 0,
+  ]: number[] = samp.callNative("GetObjectAttachedData", "i", objectid);
+  return { attached_vehicleid, attached_objectid, attached_playerid };
+};
+
+export const GetObjectAttachedOffset = (objectid: number): ObjectRotPosImpl => {
+  const [
+    fX = 0.0,
+    fY = 0.0,
+    fZ = 0.0,
+    fRotX = 0.0,
+    fRotY = 0.0,
+    fRotZ = 0.0,
+  ]: number[] = samp.callNative("GetObjectAttachedOffset", "i", objectid);
+  return { fX, fY, fZ, fRotX, fRotY, fRotZ };
+};
+
+export const GetObjectSyncRotation = (objectid: number): number => {
+  return samp.callNative("GetObjectSyncRotation", "i", objectid);
+};
+
+// Return values: 1 = material, 2 = material tex
+export const IsObjectMaterialSlotUsed = (
+  objectid: number,
+  materialindex: number
+) => {
+  return samp.callNative(
+    "IsObjectMaterialSlotUsed",
+    "ii",
+    objectid,
+    materialindex
+  );
+};
+
+export const GetObjectMaterial = (
+  objectid: number,
+  materialindex: number
+): MaterialImpl => {
+  let [modelid, txdname, texturename, materialcolor]: [
+    number,
+    string,
+    string,
+    number | string
+  ] = samp.callNative(
+    "GetObjectMaterial",
+    "iiISiSiI",
+    objectid,
+    materialindex,
+    64,
+    64
+  );
+  materialcolor = materialcolor.toString(16);
+  return { modelid, txdname, texturename, materialcolor };
+};
+
+export const GetObjectMaterialText = (
+  objectid: number,
+  materialindex: number
+): MaterialTextImpl => {
+  let [
+    text,
+    materialsize,
+    fontface,
+    fontsize,
+    bold,
+    fontcolor,
+    backcolor,
+    textalignment,
+  ]: [
+    string,
+    number,
+    string,
+    number,
+    number,
+    number | string,
+    number | string,
+    number
+  ] = samp.callNative(
+    "GetObjectMaterialText",
+    "iiSiISiIIIII",
+    objectid,
+    materialindex,
+    2048,
+    32
+  );
+  fontcolor = fontcolor.toString(16);
+  backcolor = backcolor.toString(16);
+  return {
+    text,
+    materialsize,
+    fontface,
+    fontsize,
+    bold,
+    fontcolor,
+    backcolor,
+    textalignment,
+  };
+};
+
+export const IsObjectNoCameraCol = (objectid: number): boolean => {
+  return Boolean(samp.callNative("IsObjectNoCameraCol", "i", objectid));
+};
